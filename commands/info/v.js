e8e55aca98700e96c6a5c7a7104779256f8f2c03
TM0D <tmodder24@gmail.com>
2021-07-16 16:04:02 -0500

    add version command

---
+++ commands/info/v.js
@@ -0,0 +1,21 @@
+const { mode, author, botAdmin, dev } = require("./../../config.json")
+
+module.exports = {
+	name: 'v',
+	description: 'display version info',
+    args : false,
+	execute(message, args) { 
+        if(message.author == author || message.botAdmin == botAdmin) {
+            message.channel.send("Bot Admin:\nDEV MODE: `" + dev + "`\nINTENDED SERVER: `" + mode + "`")
+        }
+		switch (mode) {
+            case 1:
+                return message.channel.send("Current Version: `CUTE v0.1`")
+                break;
+            case 0:
+            default:
+                return message.channel.send("Current Version: `UNKNOWN` (dev version?)");
+                break;
+        }
+	},
+};
